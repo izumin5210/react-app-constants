d38e42c36d74570291c1eeab943d8047d1f839f1
izumin5210 <m@izum.in>
2020-01-27 01:31:19 +0900

    impl react-app-constants

---
+++ packages/react-app-constants/src/index.tsx
@@ -0,0 +1,59 @@
+/* eslint eqeqeq: ["error", "always", {"null": "ignore"}] */
+
+import { Base64 } from "js-base64";
+import React, { createContext, useContext, Context } from "react";
+
+const metaName = "app-constants";
+
+export default function createConstantsContext<T>(): {
+  context: Context<T | null>;
+  useConstants: () => T;
+  ConstantsProvider: React.FC<{ constants?: T }>;
+} {
+  const context = createContext<T | null>(null);
+  const useConstants = createHook(context);
+
+  const ConstantsProvider: React.FC<{ constants?: T }> = ({
+    children,
+    constants,
+  }) => {
+    const value = constants || loadConstants<T>();
+    return <context.Provider value={value}>{children}</context.Provider>;
+  };
+
+  return { context, ConstantsProvider, useConstants };
+}
+
+function createHook<T>(ctx: Context<T | null>): () => T {
+  return () => {
+    const consts = useContext(ctx);
+    if (consts == null) {
+      throw Error("constans is not loaded");
+    }
+    return consts;
+  };
+}
+
+function loadConstants<T>(): T {
+  if (typeof window !== "undefined" && typeof window.document !== "undefined") {
+    return loadConstantsFromMeta();
+  }
+  return loadConstantsFromEnv();
+}
+
+function loadConstantsFromMeta<T>(): T {
+  const v = document
+    .querySelector(`meta[name="${metaName}"]`)
+    ?.getAttribute("content");
+  if (v == null) {
+    throw new Error(`meta[name="${metaName}"] is not found or has no contents`);
+  }
+  return JSON.parse(Base64.decode(v)) as T;
+}
+
+function loadConstantsFromEnv<T>(): T {
+  const entries = Object.entries(process.env)
+    .filter(([k, _]) => k.startsWith("REACT_APP_"))
+    .map(([k, v]) => [k.replace(/^REACT_APP_/, ""), v]);
+  return Object.fromEntries(entries) as T;
+}
