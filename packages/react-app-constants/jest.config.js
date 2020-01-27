4dbcc07de4ce75041d5ccbb8418dcc3afdf96c93
izumin5210 <m@izum.in>
2020-01-27 16:41:05 +0900

    chore(all): Publish built with ES6 and CommonJS

--- packages/react-app-constants/jest.config.js
+++ packages/react-app-constants/jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   ...require("../../jest.config"),
   testEnvironment: "jest-environment-jsdom-fourteen",
-  globals: {
-    "ts-jest": {
-      tsConfig: "./tsconfig.test.json",
-    },
-  },
 };
